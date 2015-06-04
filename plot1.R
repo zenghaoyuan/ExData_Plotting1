9308f182d2f4eec59fd7c6e903bfac426c41e32a
zenghy <1298256517@qq.com>
2015-06-04 14:30:25 +0800

    complete

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
-plot1<-function(){
-        
-}
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.string="?")
+data<-transform(data,Date=as.character(Date),Time=as.character(Time))
+dataset<-data[data[,"Date"]=="1/2/2007"|data[,"Date"]=="2/2/2007",]
+png(file="plot1.png",width = 480, height = 480, units = "px")
+hist(dataset[,"Global_active_power"],xlab="Global Active Power(kilowatts)",ylab="Frequency",main="Global Active Power",col="red")
+dev.off()
